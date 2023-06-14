97b8118fb80f85a48b65779f439a83fb97e8b4c8
Viktorline <viktorlinework@gmail.com>
2023-06-14 08:16:15 +0700

    made normal routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,18 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
 import HomePage from './components/HomePage';
 
-const CheckToken = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    localStorage.getItem('token') ? navigate('/') : navigate('/login');
-  }, [navigate]);
-
-  return null;
-};
-
 const App = () => {
+  const token = localStorage.getItem('token');
+
   return (
     <BrowserRouter>
-      <CheckToken />
       <Routes>
-        <Route path='/' element={<HomePage />} />
+        <Route path='/' element={token ? <Navigate to='/home' /> : <Navigate to='/login' />} />
+        <Route path='/home' element={<HomePage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<RegisterPage />} />
       </Routes>
